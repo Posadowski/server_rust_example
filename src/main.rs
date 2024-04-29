08b0c0aade1661b784ed101c728e90f6402a0b82
Posadowski <mateuszposa@gmail.com>
2024-04-29 12:32:50 +0200

    feat: search files automatically based on enpoint

--- src/main.rs
+++ src/main.rs
@@ -1,46 +1,51 @@
-use std::net::{TcpListener, TcpStream};
 use std::fs::File;
-use std::path::Path;
 use std::io::{Read, Write};
-
+use std::net::{TcpListener, TcpStream};
+use std::path::Path;
 
 fn handle_client(mut stream: TcpStream) {
     let mut buffer = [0; 1024];
     match stream.read(&mut buffer) {
         Ok(_) => {
-            println!("Received request from client: {}", String::from_utf8_lossy(&buffer));
-            let response = if buffer.starts_with(b"GET /en/") {
-                if buffer.starts_with(b"GET /en/ ") || buffer.starts_with(b"GET /en/index ") {
-                    read_file_response("pages/en/index.html", "text/html")
-                } else if buffer.starts_with(b"GET /en/contact ") {
-                    read_file_response("pages/en/contact.html", "text/html")
-                } else if buffer.starts_with(b"GET /en/about ") {
-                    read_file_response("pages/en/about.html", "text/html")
+            println!(
+                "Received request from client: {}",
+                String::from_utf8_lossy(&buffer)
+            );
+            let endpoint_to_check = String::from_utf8_lossy(&buffer);
+            let mut trimmed_path = String::new();
+            if let Some(path) = endpoint_to_check.splitn(2, "GET ").nth(1) {
+                if let Some(end) = path.find(" HTTP/") {
+                    trimmed_path = path[..end].to_string(); // Assign value inside the block
+                    println!("{}", trimmed_path);
                 } else {
-                    not_found_response()
+                    println!("String not found 'HTTP/'");
                 }
-            } else if buffer.starts_with(b"GET /pl/") {
-                if buffer.starts_with(b"GET /pl/ ") || buffer.starts_with(b"GET /pl/index ") {
-                    read_file_response("pages/pl/index.html", "text/html")
-                } else if buffer.starts_with(b"GET /pl/contact ") {
-                    read_file_response("pages/pl/contact.html", "text/html")
-                } else if buffer.starts_with(b"GET /pl/about ") {
-                    read_file_response("pages/pl/about.html", "text/html")
+            } else {
+                println!("String not found 'GET '");
+            }
+
+            let response = if trimmed_path.starts_with("/") {
+                if trimmed_path.len() > "/".len() {
+                    // Zliczanie za pomocą metody matches()
+                    let count_matches = trimmed_path.matches('/').count();
+                    println!("Liczba znaków '/' (matches()): {}", count_matches);
+                    if count_matches > 1 {
+                        if trimmed_path.ends_with("/") {
+                            // Jeśli tak, wywołaj funkcję z odpowiednimi argumentami
+                            let file_path = format!("pages{}index.html", trimmed_path);
+                            read_file_response(&file_path, "text/html")
+                        } else {
+                            let path_to_find = format!("pages{}.html", trimmed_path);
+                            read_file_response(&path_to_find, "text/html")
+                        }
+                    }else {
+                        let path_to_find = format!("pages{}.html", trimmed_path);
+                        read_file_response(&path_to_find, "text/html")
+                    }
                 } else {
-                    not_found_response()
+                    //let path_to_find = format!("pages{}.html", trimmed_path);
+                    read_file_response("pages/index.html", "text/html")
                 }
-            }else if buffer.starts_with(b"GET /de/") {
-                if buffer.starts_with(b"GET /de/ ") || buffer.starts_with(b"GET /de/index ") {
-                    read_file_response("pages/de/index.html", "text/html")
-                } else if buffer.starts_with(b"GET /de/contact ") {
-                    read_file_response("pages/de/contact.html", "text/html")
-                } else if buffer.starts_with(b"GET /de/about ") {
-                    read_file_response("pages/de/about.html", "text/html")
-                } else {
-                    not_found_response()
-                }
-            } else if buffer.starts_with(b"GET /")  {
-                    read_file_response("pages/index.html", "text/html") 
             } else {
                 not_found_response()
             };
@@ -59,13 +64,18 @@ fn handle_client(mut stream: TcpStream) {
 }
 
 fn read_file_response(file_path: &str, content_type: &str) -> String {
-    let file_content = read_file(file_path).unwrap_or_else(|_| String::from("404 Not Found"));
-    format!("HTTP/1.1 200 OK\r\nContent-Type: {}\r\n\r\n{}", content_type, file_content)
+    println!("try to find {}", file_path);
+    let file_content = read_file(file_path).unwrap_or_else(|_| not_found_response());
+    format!(
+        "HTTP/1.1 200 OK\r\nContent-Type: {}\r\n\r\n{}",
+        content_type, file_content
+    )
 }
 
 fn not_found_response() -> String {
-    let not_found_content = read_file("pages/404.html").unwrap_or_else(|_| String::from("404 Not Found"));
-    format!("HTTP/1.1 404 Not Found\r\nContent-Type: text/html\r\n\r\n{}", not_found_content)
+    let not_found_content =
+        read_file("pages/en/404.html").unwrap_or_else(|_| String::from("404 Not Found"));
+    format!("{}", not_found_content)
 }
 
 fn read_file(path: &str) -> std::io::Result<String> {
